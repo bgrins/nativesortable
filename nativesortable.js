b538d833d055b0bd208005d331d8ec779d04555d
bgrins <briangrinstead@gmail.com>
2012-02-17 20:07:59 -0600

    wow, all that messing with context and just needed a css rule

--- nativesortable.js
+++ nativesortable.js
@@ -27,6 +27,18 @@ nativesortable = (function() {
         }
     }
     
+    function matchesSelector(el, selector) {
+        if (el.matchesSelector)
+            return el.matchesSelector(selector);
+        if (el.webkitMatchesSelector)
+            return el.webkitMatchesSelector(selector);
+        if (el.mozMatchesSelector)
+            return el.mozMatchesSelector(selector);
+        if (el.msMatchesSelector)
+            return el.msMatchesSelector(selector);
+        return false;
+    }
+    
     function isBelow(el1, el2) {
     
         var parent = el1.parentNode;
@@ -44,86 +56,115 @@ nativesortable = (function() {
         return false;
     }
     
-    var currentlyDraggingElement = null;
-    var placeholder = null;
-    
-    function handleDragStart(e) {
-    
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', e.target.innerHTML);
-
-        currentlyDraggingElement = e.target;
-        addClassName(currentlyDraggingElement, 'moving');
-        
-        // Code to deal with placeholders (see demo here for an idea: http://jqueryui.com/demos/sortable/)
-        //placeholder = currentlyDraggingElement.cloneNode();
-        //placeholder.style.visibility = "hidden";
-        //placeholder.removeAttribute("draggable");
-        //addClassName(placeholder, "placeholder");
-        
-    }
-    function handleDragOver(e) {
-    
-        if (e.preventDefault) {
-            e.preventDefault();
+    function closest(child, selector) {
+        var cur = child;
+        while (cur) {
+            if (matchesSelector(cur, selector)) {
+                return cur;
+            }
+            cur = cur.parentNode;
+            if ( !cur || !cur.ownerDocument || cur.nodeType === 11 ) {
+                break;
+            }
         }
-        
-        e.dataTransfer.dropEffect = 'move';
-        
-        //e.target.parentNode.insertBefore(placeholder, e.target.nextSibling);
-        return false;
+        return null;
     }
-    
-    function handleDragEnter(e) {
-        if (currentlyDraggingElement != e.target) {
-            addClassName(e.target, 'over');
-        }
-    }
-    
-    function handleDragLeave(e) {
-        removeClassName(e.target, 'over');
-    }
-    
-    function handleDrop(e) {
-        if (e.stopPropagation) {
-            e.stopPropagation();
-        }
-        
-        if (isBelow(currentlyDraggingElement, e.target)) {
-            // Insert before.
-            e.target.parentNode.insertBefore(currentlyDraggingElement, e.target);
-        
-        }
-        else {
-            // Insert after.
-            e.target.parentNode.insertBefore(currentlyDraggingElement, e.target.nextSibling);
-        }
-        
-        console.log(isBelow(currentlyDraggingElement, e.target));
-        //placeholder.parentNode.removeChild(placeholder);
-    }
-    
-    function matchesSelector(el, selector) {
-        if (el.matchesSelector)
-            return el.matchesSelector(selector);
-        if (el.webkitMatchesSelector)
-            return el.webkitMatchesSelector(selector);
-        if (el.mozMatchesSelector)
-            return el.mozMatchesSelector(selector);
-        if (el.msMatchesSelector)
-            return el.msMatchesSelector(selector);
-        return false;
-    }
-    
-
     
     return function(element, childSelector) {
     
+        
+        var currentlyDraggingElement = null;
+        var placeholder = null;
+        
+        function handleDragStart(e) {
+        
+            e.dataTransfer.effectAllowed = 'move';
+            e.dataTransfer.setData('Text', "*"); // Need to set to something
+            
+            currentlyDraggingElement = this;
+            addClassName(currentlyDraggingElement, 'moving');
+            
+            // Code to deal with placeholders (see demo here for an idea: http://jqueryui.com/demos/sortable/)
+            //placeholder = currentlyDraggingElement.cloneNode();
+            //placeholder.style.visibility = "hidden";
+            //placeholder.removeAttribute("draggable");
+            //addClassName(placeholder, "placeholder");
+            
+        }
+        function handleDragOver(e) {
+            if (!currentlyDraggingElement) {
+                return true;
+            }
+            
+            if (e.preventDefault) {
+                e.preventDefault();
+            }
+            
+            //e.dataTransfer.dropEffect = 'move';
+            
+            //e.target.parentNode.insertBefore(placeholder, e.target.nextSibling);
+            return false;
+        }
+        
+        function handleDragEnter(e) {
+            if (!currentlyDraggingElement) {
+                return true;
+            }
+            
+            if (e.preventDefault) {
+                e.preventDefault();
+            }
+            
+            addClassName(this, 'over');
+            
+            return false;
+        }
+        
+        function handleDragLeave(e) {
+            removeClassName(this, 'over');
+        }
+        
+        function handleDrop(e) {
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            }
+            if (e.preventDefault) {
+                e.preventDefault();
+            }
+            
+            if (isBelow(currentlyDraggingElement, this)) {
+                // Insert before.
+                this.parentNode.insertBefore(currentlyDraggingElement, this);
+            
+            }
+            else {
+                // Insert after.
+                this.parentNode.insertBefore(currentlyDraggingElement, this.nextSibling);
+            }
+            
+            //placeholder.parentNode.removeChild(placeholder);
+        }
+        
         function delegate(fn) {
             return function(e) {
-                if (!hasClassName(e.target, "placeholder") && matchesSelector(e.target, childSelector)) {
-                    fn.apply(this, [e]);   
+            
+                // Images and links are draggable by default.  Make them trigger events for the parent.
+                if (matchesSelector(e.target, childSelector)) {
+                    fn.apply(e.target, [e]);
                 }
+                /*
+                else if (e.target.tagName === "IMG" || e.target.tagName === "A") {
+                    context = closest(e.target, childSelector);
+                    
+                    if (context) {
+                        fn.apply(context, [e]);
+                        
+                        if (e.type == "dragover" || e.type == "dragleave") {
+                            addClassName(context, 'over');
+                        }
+                    }
+                }
+                */
             }
         }
         
@@ -132,7 +173,7 @@ nativesortable = (function() {
             //[].forEach.call(element.querySelectorAll(".placeholder"), function(el) {
             //    el.parentNode.removeChild(el);
             //});
-            
+            currentlyDraggingElement = null;
             [].forEach.call(element.querySelectorAll(childSelector), function(el) {
                 removeClassName(el, 'over');
                 removeClassName(el, 'moving');
@@ -147,7 +188,6 @@ nativesortable = (function() {
         element.addEventListener('dragend', delegate(handleDragEnd), false);
 
         [].forEach.call(element.querySelectorAll(childSelector), function(el) {
-            
             el.setAttribute("draggable", "true");
         });
     }
